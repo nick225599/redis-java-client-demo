361266e0a50976487d3c3ce62d6fffe1d8bc4506
_______S_cs <108015636+nick225599@users.noreply.github.com>
2023-05-15 22:17:58 +0800

    和redis交互得用 utf-8

--- src/test/java/com/companyname/redisjavaclientdemo/MemoryInput.java
+++ src/test/java/com/companyname/redisjavaclientdemo/MemoryInput.java
@@ -1,14 +1,46 @@
 package com.companyname.redisjavaclientdemo;
 
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 
 public class MemoryInput {
-    public static void main(String[] args) throws IOException {
-        StringReader in = new StringReader("hello风雨📖欲来花满楼\u0419FDم");
-        int c;
-        while((c = in.read()) != -1){
-            System.out.print((char)c);
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        StringReader in = new StringReader("hello风雨📖欲来花满楼\u0419FDم");
+//        int c;
+//        while((c = in.read()) != -1){
+//            System.out.print((char)c);
+//        }
+
+        // char 与 int 是通过 unicode 字符集强转的
+        Character c  = 'م';
+        System.out.println(Integer.toHexString((int)(c)));
+        System.out.println("\u0645");
+
+        // 字节流到字符流的转换也需要制定编码格式,📖 不能转成一个字符，所以还是会乱码
+        String charsetName = "utf-8";
+        InputStream in2 = new ByteArrayInputStream(
+                "hello风雨📖欲来花满楼\u0419FDم".getBytes(charsetName));
+        Reader reader = new InputStreamReader(in2, charsetName);
+        int i2;
+        while((i2 = reader.read()) != -1){
+            System.out.print((char)i2);
+            System.out.print(", ");
         }
+        System.out.println();
+        System.out.println("----");
+
+        // 直接用字节流使用同样的字符集写入写出，还原成字符串就可以保留 📖 完好无损
+        // 所以字符串里存储的是 byte[] 而不是 char[]
+        in2 = new ByteArrayInputStream(
+                "hello风雨📖欲来花满楼\u0419FDم".getBytes(charsetName));
+        byte b;
+        byte[] bytes = new byte[in2.available()];
+        int i = 0;
+        while((b = (byte)in2.read()) != -1){
+            bytes[i++] = b;
+            System.out.print(b);
+            System.out.print(", ");
+        }
+        System.out.println();
+        System.out.println(new String(bytes, charsetName));
     }
 }
